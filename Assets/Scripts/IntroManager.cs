48baf3f323cfec7583234b16bf4ce0d5fb4b8649
hwoongg <gusdnd8536@gmail.com>
2020-11-09 10:06:30 +0900

    로비 해상도 변경, 서강대 로고 인트로 추가

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] Image logo;
+    [SerializeField] float fadeSpeed = 0.5f;
+
+    bool isFadeIn;
+    
+
+    Color startColor;
+    Color destColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startColor = new Color(1, 1, 1, 0);
+        destColor = new Color(1, 1, 1, 1);
+
+        StartCoroutine(LogoAnimRoutine());
+    }
+
+    
+    IEnumerator LogoAnimRoutine()
+    {
+        yield return StartCoroutine(LogoFade(startColor, destColor));
+
+        yield return new WaitForSeconds(3.0f);
+
+        yield return StartCoroutine(LogoFade(destColor, startColor));
+
+        SceneManager.LoadScene("Title");
+    }
+    IEnumerator LogoFade(Color _start, Color _des)
+    {
+        float lerpVal = 0;
+        while (true)
+        {
+            logo.color = Color.Lerp(_start, _des, lerpVal);
+            lerpVal += Time.deltaTime * fadeSpeed;
+            if (lerpVal > 1)
+            {
+                lerpVal = 1;
+                break;
+            }
+            yield return null;
+        }
+        yield break;
+    }
+}
